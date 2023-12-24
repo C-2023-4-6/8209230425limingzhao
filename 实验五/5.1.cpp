830369e78bc38a588e590eb5f841d028209eade9
9917758 <150009820+9917758@users.noreply.github.com>
2023-12-24 12:38:08 +0800

    Add files via upload

---
+++ 实验五/5.1.cpp
@@ -0,0 +1,27 @@
+//#include<iostream>
+//using namespace std;
+//class Time             // 定义Time类
+//{
+//public:              // 数据成员该为私用的
+//	void set_time(){
+//		cin >> hour;
+//		cin >> minute;
+//		cin >> sec;
+//	}
+//	void show_time() {
+//		cout << hour << "; " << minute << "; " << sec << "; ";
+//	}
+//
+//
+//private:
+//	int hour;
+//	int minute;
+//	int sec;
+//};
+//Time t1;//实例化
+//int main()
+//{
+//	t1.set_time();
+//	t1.show_time();
+//	return 0;
+//}
---
+++ 实验五/5.3.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+//using namespace std;
+//class v {
+//private:
+//	int length;
+//	int width;
+//	int hight;
+//public:
+//	void set_value() {
+//		
+//		cin >> length;
+//		cin >> width;
+//		cin >> hight;
+//	
+//	}
+//	void show_valume() {
+//		int valume;
+//		valume = length * width * hight;
+//		cout << valume;
+//	}
+//};
+//v volume;
+//int main() {
+//	volume.set_value();
+//	volume.show_valume();
+//
+//	return 0;
+//
+//}
